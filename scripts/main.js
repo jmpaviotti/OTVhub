b65c988af2b4ed32fafb7319ecd1ea2b51be7639
unknown <jmpaviotti@gmail.com>
2020-05-23 00:00:48 -0300

    Moved grid creation code to JS file, auto resize

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,53 @@
-const options = [], players = [], channel_names = ['fedmyster', 'fedmyster2', 'lilypichu', 'scarra', 'pokimane', 'yvonnie'];
+// Functions
+function insertPlayer(name, container) {
+    // Inserts the html elements required for a twitch player embed into DOM
+    const parent = document.createElement("div");
+    const child = document.createElement("div");
+
+    parent.setAttribute("class", "grid-item");
+    child.setAttribute("id", "player_" + name);
+
+    parent.append(child);
+    container.append(parent);
+}
+
+function calcPlayerDimensions() {
+    // Calculates size of twitch player embeds based on the main container
+    const box = document.querySelector('#twitch');
+    return {width: box.offsetWidth * 0.32, height: box.offsetHeight * 0.46}
+}
+
+function resizePlayer(player, dimensions) {
+    // Resizes a twitch player embed
+    player.setHeight(dimensions.height);
+    player.setWidth(dimensions.width);
+}
+
+// Variables
+const options = [], players = [], channel_names = ['fedmyster', 'fedmyster2', 'lilypichu', 'pokimane', 'scarra', 'yvonnie'];
+const container = document.querySelector("#twitch");
 let default_volume = 0.8;
 
-for(let i = 0; i < channel_names.length; i++) {    
+// Create players and add them to list
+for(let i = 0; i < channel_names.length; i++) {  
+
+    insertPlayer(channel_names[i], container) // Inserts twitch player boxes into DOM
+
     options.push({
-        width: 400,
-        height: 300,
-        channel: channel_names[i],
+        ...calcPlayerDimensions(),
+        channel: channel_names[i]
     });
 
-    players.push( new Twitch.Player('player_' + channel_names[i], options[i]) )
-};
+    players.push( new Twitch.Player('player_' + channel_names[i], options[i]) );
+}
 
+// Autopause players
 players.forEach(player => player.addEventListener(Twitch.Player.READY, function() {
     player.pause();
 }));
 
-
-
-
+// Resize players when user resizes window
+window.onresize = function() {
+    const dimensions = calcPlayerDimensions();
+    players.forEach(player => resizePlayer(player, dimensions));     
+}
