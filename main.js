c1bb91d2a5b72633a0ad2dd9121aad3d524551ae
unknown <jmpaviotti@gmail.com>
2020-05-29 18:26:01 -0300

    Remade HTML/CSS, added hamburger menu

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules
-import { autoPauseAll, clearContainer } from './modules/twitch.js';
+import { autoPauseAll, insertChat, clearContainer } from './modules/twitch.js';
 import * as grid from './modules/grid.js';
 import * as single from './modules/single.js';
 
@@ -21,34 +21,20 @@ let mode = 'single';
 
 // Main code
 // Choose between grid and dynamic view (Eventually change to dynamic import?)
-if (mode == 'grid') {
-  // Grid mode
-  grid.createPlayerGrid(channel_names, container, players);
-  window.addEventListener(
-    'resize',
-    () => grid.resizeAll(players, container),
-    false
-  );
-} else if (mode == 'single') {
+
+if (mode == 'single') {
   // Single player mode
   single.createPlayerBox(channel_names, container, players);
-  single.resizeGrid(container);
-  window.addEventListener(
-    'resize',
-    () => {
-      single.resizeAll(players, container);
-      single.resizeGrid(container);
-    },
-    false
-  );
+  insertChat(channel_names[0], container);
+} else if (mode == 'grid') {
+  // Grid mode
+  grid.createPlayerGrid(channel_names, container, players);
+  autoPauseAll(players);
 }
 
-// Global actions
-autoPauseAll(players);
-
 // Switch
-button.onclick = function () {
-  clearContainer(container);
-  //   if (mode == 'grid') grid.createPlayerGrid(channel_names, container, players);
-  //   else if (mode == 'single');
-};
+// button.onclick = function () {
+//   clearContainer(container);
+//   if (mode == 'grid') grid.createPlayerGrid(channel_names, container, players);
+//   else if (mode == 'single');
+// };
--- modules/twitch.js
+++ modules/twitch.js
@@ -1,13 +1,21 @@
-/* global Twitch*/
-
 function insertPlayer(name, container) {
   // Inserts the html elements required for a twitch player embed into DOM
-  const parent = document.createElement('div');
   const child = document.createElement('div');
-
-  parent.setAttribute('class', 'grid-item');
   child.setAttribute('id', 'player_' + name);
+  child.setAttribute('class', 'twitch-player');
+  container.querySelector('#players').append(child);
+}
 
+function insertChat(name, container) {
+  const parent = document.createElement('aside');
+  const child = document.createElement('iframe');
+  parent.setAttribute('id', 'chat');
+  child.setAttribute(
+    'src',
+    `https://www.twitch.tv/embed/${name}/chat?parent=otvhub.com`
+  );
+  child.setAttribute('frameborder', '0');
+  child.setAttribute('scrolling', 'yes');
   parent.append(child);
   container.append(parent);
 }
@@ -36,4 +44,4 @@ function autoPauseAll(players) {
   );
 }
 
-export { insertPlayer, resizePlayer, clearContainer, autoPauseAll };
+export { insertPlayer, insertChat, resizePlayer, clearContainer, autoPauseAll };
